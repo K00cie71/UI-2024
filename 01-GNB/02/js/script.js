a6efb5e726b4f6e8d20447ceb177f5d1ba0e98e1
K00cie71 <munm79871@gmail.com>
2024-10-04 10:55:25 +0900

    삼항조건연산자

--- 01-GNB/02/js/script.js
+++ 01-GNB/02/js/script.js
@@ -20,14 +20,20 @@ $(function () {
 
     // 마우스 휠을 조작했을 때
     $window.on("wheel", function (e) {
-        console.log(e);
+        // console.log(e);
 
-        if (e.originalEvent.wheelDelta > 0) {
-            // 휠을 올렸을때
-            $header.removeClass("hide");
-        } else {
-            // 휠을 내렸을때
-            $header.addClass("hide");
-        }
+        // if (e.originalEvent.wheelDelta > 0) {
+        //     // 휠을 올렸을때
+        //     $header.removeClass("hide");
+        // } else {
+        //     // 휠을 내렸을때
+        //     $header.addClass("hide");
+        // }
+
+        // 삼항조건연산자
+        // 조건 ? 참일때 동작 : 거짓일때 동작
+        e.originalEvent.wheelDelta > 0
+            ? $header.removeClass("hide")
+            : $header.addClass("hide");
     });
 });
